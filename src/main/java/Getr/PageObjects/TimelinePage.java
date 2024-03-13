f9bbc57f2c370258298b3e517fda0730ba28c5d1
Manu <manojmgowda43@gmail.com>
2024-03-13 09:38:34 +0530

    rf

--- src/main/java/Getr/PageObjects/TimelinePage.java
+++ src/main/java/Getr/PageObjects/TimelinePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+
 import Getr.AbstractComponents.AbstractComponents;
 
 public class TimelinePage extends AbstractComponents{
